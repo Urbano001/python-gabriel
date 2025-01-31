c16901734c29ae694d801045980f73a7b2ea3152
urikrhe <gabouricare@gmail.com>
2025-01-31 10:40:51 -0400

    final 31-1-25

---
+++ metodo.py
@@ -0,0 +1,12 @@
+#funciones vemos el funcion y le pasamos un valor def suma(): return de la funcion
+#metodo Tenemos la variable un punto y el nombre de la funcion variable.funcion()
+
+ciudad= 'Caracas'
+def mostrar_ciudad(ciudad):
+    print(f'Muestrame la ciudad de {ciudad}')
+
+mostrar_ciudad('Maracay')
+print( ciudad.lower())
+print( ciudad.capitalize())
+
+       
---
+++ operadores.py
@@ -0,0 +1,19 @@
+print(2+2)
+print(3*3)
+print(2/2)
+
+print(2**5)
+print(4**3)#cuantas veces se va a multiplicar la base - POTENCIA
+
+#python soporta el orden de las operaciones
+
+print((2+3)*5)
+
+#incremento
+
+numero = 50
+print(numero)
+
+#incrementar
+numero += 5
+print(numero)
